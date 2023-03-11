826e0cf44a8a248ac55e3cd3171238e158e9dbb2
R. Alejandro Fuentes <fuentesra@hotmail.com>
2023-03-11 20:23:51 -0300

    Add csvtojson for read csv file

--- node_read_file/index.js
+++ node_read_file/index.js
@@ -1,7 +1,16 @@
-import Chalk from 'chalk';
+import chalk from 'chalk'
+import csv from 'csvtojson'
+
 
 function testing() {
-    console.log(Chalk.red(`Hello\nusing ${process.version} node`));
+    console.log(chalk.red(`Hello\nusing ${process.version} node`));
+    const converter=csv()
+        .fromFile('./Data.csv')
+        .then((json)=>
+            {
+                console.log(json);
+            });
 }
+
 
 testing();
